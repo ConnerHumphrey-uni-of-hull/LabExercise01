190f124893a549652db87fcee639a9419fde5a24
ConnerHumphrey-uni-of-hull <ConnerHumphrey-uni-of-hull@users.noreply.github.com>
2020-10-08 15:08:37 +0100

    Finished the Lab

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using System.IO;
+
 
 namespace LabExercise01
 {
@@ -6,7 +9,45 @@ namespace LabExercise01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            string data = null;
+            using (var reader = new StreamReader("input.txt"))
+            {
+                data = reader.ReadToEnd();
+            }
+
+            // converts the each char in the sting to lower
+            data = data.ToLower();
+            
+            
+            var sb = new StringBuilder();
+            foreach (char c in data)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+                
+            }
+            data = sb.ToString();
+
+            string [] dataArray = data.Split(" ");
+           
+
+            Array.Sort(dataArray);
+         
+
+            data = string.Join(" ", dataArray);
+
+            Console.WriteLine(data);
+
+            
+            using (var writer = new StreamWriter(@"E:\software engineering labs\LabExercise01\output.txt"))
+            {
+                writer.Write(data);
+            }
+
+
+
+
+
+    }
     }
 }
